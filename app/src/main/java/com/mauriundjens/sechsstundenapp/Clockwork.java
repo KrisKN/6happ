8e528ad1f9a6f0e13bf9d0b736ec59e92cba8e71
KrisKN <jg@pegtop.net>
2016-12-09 16:51:42 +0100

    Time counter logic added.

---
+++ app/src/main/java/com/mauriundjens/sechsstundenapp/Clockwork.java
@@ -0,0 +1,75 @@
+package com.mauriundjens.sechsstundenapp;
+
+public class Clockwork
+{
+    private long offsetMillis = 0;
+    private long startMillis;
+    private int speed = 0;
+
+    public Clockwork()
+    {
+    }
+
+    private long calcMillis(long timeMillis)
+    {
+        return offsetMillis + (timeMillis - startMillis) * speed;
+    }
+
+    private void update()
+    {
+        // time elapsed since last update is recorded,
+        // must be called before parameter changes or serialization
+        long currentMillis = System.currentTimeMillis();
+        offsetMillis = calcMillis(currentMillis);
+        startMillis = currentMillis;
+    }
+
+    public void start(int speed)
+    {
+        update();
+        this.speed = speed;
+    }
+
+    public void start()
+    {
+        update();
+        speed = 1;
+    }
+
+    public void stop()
+    {
+        update();
+        speed = 0;
+    }
+
+    public void reset()
+    {
+        speed = 0;
+        offsetMillis = 0;
+    }
+
+    public void setSpeed(int value)
+    {
+        update();
+        speed = value;
+    }
+
+    public int getSpeed()
+    {
+        return speed;
+    }
+
+    public long getMillis()
+    {
+        return calcMillis(System.currentTimeMillis());
+    }
+
+    public String toString()
+    {
+        long millis = getMillis();
+        long s = (millis / 1000) % 60;
+        long m = (millis / 60000) % 60;
+        long h = millis / 3600000;
+        return String.format("%d:%02d:%02d", h, m, s);
+    }
+}
